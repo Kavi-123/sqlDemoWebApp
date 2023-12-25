d91d5f915024a9afbfec3a5dc929c1a5aef743e4
Kavi-123 <31906657+Kavi-123@users.noreply.github.com>
2023-12-25 18:24:52 +0530

    Add project files.

---
+++ sqlDemoWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using sqlDemoWebApp.Models;
+using sqlDemoWebApp.Services;
+
+namespace sqlDemoWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+        }
+    }
+}
